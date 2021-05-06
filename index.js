2ac1982c36df9482b6c2e0f47472c0223432a433
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-05-06 13:56:02 -0400

    Created a function to select the team member's role. Also added function to determine the changing data variable based up team member's role.

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
 const inquirer = require('inquirer');
 const fs = require('fs')
 
-const engineer = require('./lib/engineer');
-const intern = require('./lib/intern');
-const manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
+const { allowedNodeEnvironmentFlags } = require('process');
 
-console.log(fs);
-console.log(inquirer);
+const employees = [];
 
-console.log(manager.test);
-console.log(intern.test);
-console.log(engineer.test);
+// console.log(fs);
+// console.log(inquirer);
+
+// console.log(Manager.test);
+// console.log(Intern.test);
+// console.log(Engineer.test);
+
+function buildRoster() {
+    buildHTML();
+    addRole();
+}
+
+function addRole() {
+    inquirer.prompt([{
+        message: "Enter team member's name.",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "Select team member's role.",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ],
+        name: "role"
+    },
+    {
+        message: "Enter team member's employee ID number.",
+        name: "id"
+    },
+    {
+        message: "Enter team member's email address.",
+        name: "email"
+    }])
+        .then(function ({ name, role, id, email }) {
+            let roleData = "";
+            if (role === "Engineer") {
+                roleData = "GitHub username";
+            } else if (role === "Intern") {
+                roleData = "School name";
+            } else {
+                roleData = "Office number";
+            }
+            inquirer.prompt([{
+                message: `Enter team member's ${roleData}`,
+                name: "roleData"
+            },
+            {
+                type: "list",
+                message: "Would you like to add more team members?",
+                choices: [
+                    "Yes",
+                    "No"
+                ],
+                name: "moreMembers"
+            }
+            ])
+
+        })
+
+}
